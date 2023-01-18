e8470f28058807830e375ad183864af9b97b5fb6
khdfierror <khadafi1675@gmail.com>
2023-01-18 13:10:20 +0800

    add controller kamar

--- app/Http/Controllers/KamarController.php
+++ app/Http/Controllers/KamarController.php
@@ -1,10 +1,29 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Controllers\Controller;
+use App\Models\Kamar;
+
+
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Inertia\Inertia;
 
 class KamarController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $kamar = Kamar::query()
+            ->transform(fn ($item) => [
+                'id' => $item->id,
+                'name' => $item->name,
+                'total' => $item->total,
+            ]);
+
+        return Inertia::render('Kamar/Index', [
+            'kamar' => $kamar,
+
+        ]);
+    }
 }
